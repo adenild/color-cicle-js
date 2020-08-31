8b725d200c71e96032a87c7bb8ba2f5151e724e3
Adenildo Junior <adenildojunior52@gmail.com>
2020-08-31 11:21:01 -0300

    funcionou porra nenhuma das libs toma no cu
    ai eu peguei o cdn, e parei de enrolar... completei 2 tasks

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const path = require('path');
 const router = express.Router();
 const app = express();
 
+
 router.get('/', function (req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.use('/', router);
+app.use('/static', express.static('public'))
 app.listen(process.env.port || 3000);
 
 console.log("Servidor ligado");
